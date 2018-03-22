c599a748a1bca55d2a2a2701c111d5f3ff3c0429
Swen Meeuwes <swenmeeu@gmail.com>
2018-03-22 23:19:11 +0100

    Implemented ResolveSchemaReference method in SchemaHelper

--- concept-narrative-editor-framework/src/SchemaHelper.ts
+++ concept-narrative-editor-framework/src/SchemaHelper.ts
@@ -1,11 +1,30 @@
+import * as RefParser from 'json-schema-ref-parser';
+
 class SchemaHelper {
-    public static ResolveSchemaId(schemaId: string): string {
-        return schemaId.replace('#', '')
-            .replace('/', '.');
+    /**
+     * Resolves the schema reference and return the schema that is referenced
+     */
+    public static ResolveSchemaReference(schema: Object, schemaRef: string, refParser: RefParser = new RefParser()): Promise<Object> {
+        return new Promise((resolve, reject) => {
+            const schemaRefHierarchy = schemaRef.replace('#', '').split('/');
+            if (schemaRefHierarchy[0] === '')
+                schemaRefHierarchy.shift();
+
+            refParser.dereference(schema).then((dereferencedSchema) => {
+                const resolvedSchema = schemaRefHierarchy.reduce((accumulator, current) => accumulator[current], schema);
+                resolve(resolvedSchema);
+            }).catch((err) => {
+                reject(err);
+            });
+        });
     }
 
-    public static TrimPath(schemaId: string): string {
-        const splittedSchemaIdPath = schemaId.split('/');
+    /**
+     * Trims path of schema reference string 
+     * E.g. '#/definitions/foo' will return 'foo'
+     */
+    public static TrimRefPath(schemaRef: string): string {
+        const splittedSchemaIdPath = schemaRef.split('/');
         return splittedSchemaIdPath[splittedSchemaIdPath.length - 1];
     }
 }
