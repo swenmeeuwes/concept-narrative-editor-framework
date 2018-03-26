81739c0d8ba89f1ece9805b521ba33d0749dba1a
Swen <swenmeeu@gmail.com>
2018-03-26 13:49:28 +0200

    Made the electron entry point Typescript

--- concept-narrative-editor-framework/src/electron-entry-point.ts
+++ concept-narrative-editor-framework/src/electron-entry-point.ts
@@ -1,24 +1,21 @@
-const electron = require('electron')
+const electron = require('electron');
 // Module to control application life.
-const app = electron.app
+const app = electron.app;
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const BrowserWindow = electron.BrowserWindow;
 
-const path = require('path')
-const url = require('url')
+const path = require('path');
+const url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     'width': 800, 
-    'height': 600,
-    'min-height': 400,
-    'min-width': 400,
-    'web-preferences': {'web-security': false}
+    'height': 600
   });
 
   // and load the index.html of the app.
