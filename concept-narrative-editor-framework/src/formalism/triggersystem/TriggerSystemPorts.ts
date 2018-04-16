48eafc7876f6a635fd2ae0bc735d5c0c5b3b541c
Swen <swenmeeu@gmail.com>
2018-04-16 17:17:14 +0200

    Added type property to triggersystemports

--- concept-narrative-editor-framework/src/formalism/triggersystem/TriggerSystemPorts.ts
+++ concept-narrative-editor-framework/src/formalism/triggersystem/TriggerSystemPorts.ts
@@ -1,45 +1,56 @@
+// Type property must be UNIQUE
+
 const UnlockPort = {
     label: 'unlock',
+    type: 'unlock',
     group: 'in'
 };
 
 const AvailableConditionPort = {
     label: 'available',
+    type: 'available',
     group: 'in'
 };
 
 const UnlockedPort = {
     label: 'unlocked',
+    type: 'unlocked',
     group: 'out'
 };
 
 const AvailablePort = {
     label: 'available',
+    type: 'available',
     group: 'out'
 };
 
 const CompletedPort = {
     label: 'completed',
+    type: 'completed',
     group: 'out'
 };
 
 const LogicalInPort = {
     label: 'logical in',
+    type: 'logical in',
     group: 'in'
 };
 
 const LogicalOutPort = {
     label: 'logical out',
+    type: 'logical out',
     group: 'out'
 };
 
 const DelayInPort = {
     label: 'delay in',
+    type: 'delay in',
     group: 'in'
 };
 
 const DelayOutPort = {
     label: 'delay out',
+    type: 'delay out',
     group: 'out'
 };
 
