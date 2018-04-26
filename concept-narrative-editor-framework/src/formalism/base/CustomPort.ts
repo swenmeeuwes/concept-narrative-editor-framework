2eb851330f97443a872b1cdae3b4b12900265b5e
Swen <swenmeeu@gmail.com>
2018-04-26 14:19:32 +0200

    Added validation function to custom port interface

--- concept-narrative-editor-framework/src/formalism/base/CustomPort.ts
+++ concept-narrative-editor-framework/src/formalism/base/CustomPort.ts
@@ -2,6 +2,7 @@ import * as joint from 'jointjs';
 
 interface CustomPort extends joint.dia.Element.Port {
     type?: string;
+    validateConnection?: (against: SVGElement | joint.dia.CellView) => boolean;
 }
 
 export default CustomPort;
