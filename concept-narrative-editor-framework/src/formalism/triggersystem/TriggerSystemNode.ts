7009ce9c2f6dc7bb6f787753ad3c5f42dc2ad6e6
Swen <swenmeeu@gmail.com>
2018-05-15 15:57:51 +0200

    Moved port groups to seperate class

--- concept-narrative-editor-framework/src/formalism/triggersystem/TriggerSystemNode.ts
+++ concept-narrative-editor-framework/src/formalism/triggersystem/TriggerSystemNode.ts
@@ -1,72 +1,14 @@
 import * as joint from 'jointjs';
-import * as _ from 'lodash';
 
 import Node from '../base/Node';
+import { TriggerSystemPortGroups } from './TriggerSystemPorts';
 
 class TriggerSystemNode extends Node {
     constructor(attributes?: joint.dia.Element.Attributes, options?: any) {
         super(attributes, options);
 
-        this.set('markup', `<g class="content-type-node">
-                                <rect class="body" />
-                                <text class="label" />
-                            </g>`);
-
         this.prop('ports', {
-            groups: {
-                'in': {
-                    position: {
-                        name: 'left', // layout name
-                        args: {}, // arguments for port layout function, properties depends on type of layout
-                    },
-                    label: {},
-                    attrs: {
-                        '.port-body': {
-                            magnet: true
-                        }
-                    },
-                    markup: '<circle class="port-body" r="10" />', // r = radius of the circle,
-                    validateConnection: (other: SVGElement | joint.dia.CellView): boolean => {
-                        const allowConnectionsTo = ['out'];
-                        if (other instanceof SVGElement) {
-                            const otherPortGroup = other.getAttribute('port-group');
-                            return _.includes(allowConnectionsTo, otherPortGroup);
-                        }
-
-                        if (other instanceof joint.dia.CellView) {
-                            return true;
-                        }
-
-                        return false;
-                    }
-                },
-                'out': {
-                    position: {
-                        name: 'right', // layout name
-                        args: {}, // arguments for port layout function, properties depends on type of layout
-                    },
-                    label: {},
-                    attrs: {
-                        '.port-body': {
-                            magnet: true
-                        }
-                    },
-                    markup: '<circle class="port-body" r="10" />', // r = radius of the circle
-                    validateConnection: (other: SVGElement | joint.dia.CellView): boolean => {
-                        const allowConnectionsTo = ['in'];
-                        if (other instanceof SVGElement) {
-                            const otherPortGroup = other.getAttribute('port-group');
-                            return _.includes(allowConnectionsTo, otherPortGroup);
-                        }
-
-                        if (other instanceof joint.dia.CellView) {
-                            return true;
-                        }
-
-                        return false;
-                    }
-                }
-            }
+            groups: TriggerSystemPortGroups
         });
     }
 
--- concept-narrative-editor-framework/src/formalism/triggersystem/TriggerSystemPorts.ts
+++ concept-narrative-editor-framework/src/formalism/triggersystem/TriggerSystemPorts.ts
@@ -1,36 +1,93 @@
-// Type property must be UNIQUE
+import * as joint from 'jointjs';
+import * as _ from 'lodash';
 
-const UnlockPort = {
+export const TriggerSystemPortGroups = {
+    'in': {
+        position: {
+            name: 'left', // layout name
+            args: {}, // arguments for port layout function, properties depends on type of layout
+        },
+        label: {},
+        attrs: {
+            '.port-body': {
+                magnet: true
+            }
+        },
+        markup: '<circle class="port-body" r="10" />', // r = radius of the circle,
+        validateConnection: (other: SVGElement | joint.dia.CellView): boolean => {
+            const allowConnectionsTo = ['out'];
+            if (other instanceof SVGElement) {
+                const otherPortGroup = other.getAttribute('port-group');
+                return _.includes(allowConnectionsTo, otherPortGroup);
+            }
+
+            if (other instanceof joint.dia.CellView) {
+                return true;
+            }
+
+            return false;
+        }
+    },
+    'out': {
+        position: {
+            name: 'right', // layout name
+            args: {}, // arguments for port layout function, properties depends on type of layout
+        },
+        label: {},
+        attrs: {
+            '.port-body': {
+                magnet: true
+            }
+        },
+        markup: '<circle class="port-body" r="10" />', // r = radius of the circle
+        validateConnection: (other: SVGElement | joint.dia.CellView): boolean => {
+            const allowConnectionsTo = ['in'];
+            if (other instanceof SVGElement) {
+                const otherPortGroup = other.getAttribute('port-group');
+                return _.includes(allowConnectionsTo, otherPortGroup);
+            }
+
+            if (other instanceof joint.dia.CellView) {
+                return true;
+            }
+
+            return false;
+        }
+    }
+};
+
+// Type property must be UNIQUE
+export const UnlockPort = {
     label: 'unlock',
     type: 'unlock',
     group: 'in'
 };
 
-const AvailableConditionPort = {
+export const AvailableConditionPort = {
     label: 'available',
     type: 'available',
     group: 'in'
 };
 
-const UnlockedPort = {
+export const UnlockedPort = {
     label: 'unlocked',
     type: 'unlocked',
     group: 'out'
 };
 
-const AvailablePort = {
+export const AvailablePort = {
     label: 'available',
     type: 'available',
     group: 'out'
 };
 
-const CompletedPort = {
+export const CompletedPort = {
     label: 'completed',
     type: 'completed',
     group: 'out'
 };
 
-const LogicalInPort = {
+export const LogicalInPort = {
     label: 'logical in',
     type: 'logical in',
     group: 'in',
@@ -39,32 +96,20 @@ const LogicalInPort = {
     }
 };
 
-const LogicalOutPort = {
+export const LogicalOutPort = {
     label: 'logical out',
     type: 'logical out',
     group: 'out'
 };
 
-const DelayInPort = {
+export const DelayInPort = {
     label: 'delay in',
     type: 'delay in',
     group: 'in'
 };
 
-const DelayOutPort = {
+export const DelayOutPort = {
     label: 'delay out',
     type: 'delay out',
     group: 'out'
-};
-
-export { 
-    UnlockPort, 
-    AvailableConditionPort, 
-    UnlockedPort, 
-    AvailablePort, 
-    CompletedPort,
-    LogicalInPort,
-    LogicalOutPort,
-    DelayInPort,
-    DelayOutPort
 };
