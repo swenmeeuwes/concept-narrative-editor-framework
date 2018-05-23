e748e23b2e1bdfaf8ae60c2be9b8090012d75ab0
Swen <swenmeeu@gmail.com>
2018-05-23 15:58:51 +0200

    Implemented NumberProperty

---
+++ concept-narrative-editor-framework/src/properties/NumberProperty.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+
+type OnChangeEvent = (event: React.FormEvent<HTMLInputElement>) => void;
+
+interface Props {
+    value?: number;
+    onChange?: OnChangeEvent;
+}
+
+interface State {
+    value: number;
+}
+
+export default class NumberProperty extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+
+        this.state = {
+            value: props.value || 0
+        };
+    }
+
+    onChange = (event: React.FormEvent<HTMLInputElement>) => {
+        this.setState({
+            value: event.currentTarget.valueAsNumber
+        });
+
+        if (this.props.onChange)
+            this.props.onChange(event);
+    }
+
+    render() {
+        return (
+            <input type="number" className="form-control" onChange={this.onChange} value={this.state.value} />
+        );
+    }
+}
